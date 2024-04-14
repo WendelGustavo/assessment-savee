1d26b1f4ead676716cf8f711395313f6848098cf
WendelGustavo <wendelfi66@gmail.com>
2024-04-14 03:02:51 -0300

    :sparkles: feat: finished film card

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>{metadata?.title?.toString()}</title>
         <meta name="description" content={metadata?.description || ""} />
-        <link rel="icon" href="./public/img/savee-logo.png" />
+        <link rel="icon" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAAAAABXZoBIAAAAuElEQVR4Ac3SIQyDMBRF0Zt5b2bQkyjUNG52Boe3SLzAz1Uil6Aq8Qq3pK4+waG7F8CsJbPbNZCetOlPyulLv0bWDhGK9tHVQIpcfVh7QoSyjdQLYnQhuJyzkTYRghZz1BTCeIQGVVRVmRw7SRfbZJBeiGyRqrmPUYGZw9aFCFHk3RiUhw+krOsbikGaRWj2DSAsIrxrrUW1+iHCdRRvB6dPn15IumUhHaWyfpndUKZzKvb+5/UlvQEsnXa52IgEOwAAAABJRU5ErkJggg==" />
       </head>
       <body className={inter?.className + " dark"}>{children}</body>
     </html>
