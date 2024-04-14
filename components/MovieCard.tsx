0fbffe19299b214d0521f6835631a7c8af25b0b7
WendelGustavo <wendelfi66@gmail.com>
2024-04-14 03:26:47 -0300

    :sparkles: feat: finished image loading animation

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,11 +1,14 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
+import IconLoading from "./IconLoading";
 
 interface MovieCardProps {
   title: string;
   overview: string;
   posterImage: string;
   releaseDate: string;
+  loading?: boolean;
 }
 
 const MovieCard: React.FC<MovieCardProps> = ({
@@ -13,15 +16,24 @@ const MovieCard: React.FC<MovieCardProps> = ({
   overview,
   posterImage,
   releaseDate,
+  loading = true,
 }) => {
+  const [isLoading, setIsLoading] = useState(loading);
+
   return (
     <div>
       <div className="group relative rounded-lg shadow-md  dark:shadow-sm text-justify mb-5 dark:shadow-white">
         <div className="relative h-80 w-full overflow-hidden rounded-lg sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-50 sm:h-64">
+          {isLoading && (
+            <div className="absolute inset-0 bg-black bg-opacity-50 z-10 flex items-center justify-center">
+              <IconLoading />
+            </div>
+          )}
           <Image
             src={posterImage}
             alt="Movie poster image"
             className="h-full w-full object-cover object-center group-hover:scale-110 transition-transform duration-500"
+            onLoad={() => isLoading && setIsLoading(false)}
             layout="fill"
           />
         </div>
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['tailwindui.com'],
+        domains: ['tailwindui.com', 'img.freepik.com'],
+
       },
 };
 
