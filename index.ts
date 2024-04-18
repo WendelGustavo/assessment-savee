bdbc2ed8fba24fe1489c336019d2e4728e9195a3
WendelGustavo <wendelfi66@gmail.com>
2024-04-17 23:06:07 -0300

    :recycle: refactor: Re-build estructure

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import { resolvers } from './graphql/resolvers';
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(({ url }) => {
+server.listen().then(({ url }: { url: string }) => {
     console.log(`🚀 Server ready at ${url}`);
 });
   
